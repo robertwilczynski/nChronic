70547e1c5a4dcfb88ee3776a966e715d2563c651
Robert Wilczynski <robert@robertwilczynski.com>
2012-02-07 23:00:35 +0100

    AsseblyFileVersion set to match AssemblyVersion

--- src/Chronic/Properties/AssemblyInfo.cs
+++ src/Chronic/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("30e233fd-aef4-48e2-a901-a2cdb19ff6b5")]
 [assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
