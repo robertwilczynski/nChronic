139bc8e8580b181beb47b46fea8e6a5882c4d2a5
Robert Wilczynski <robert@robertwilczynski.com>
2014-05-15 09:36:33 +0200

    testing leap year handing by GRGR handler using new fluent API

--- src/Chronic.Tests/Handlers/GRGRHandler_correctly_parses.cs
+++ src/Chronic.Tests/Handlers/GRGRHandler_correctly_parses.cs
@@ -18,6 +18,26 @@ namespace Chronic.Tests.Handlers
         }
 
         [Fact]
+        public void last_day_of_next_month_when_next_month_is_February_in_leap_year()
+        {
+            When
+                .ItIs("1996-01-20")
+                .Parsing("last day of next month")
+                .ReturnsSpan()
+                .StartingAt(Time.New(1996, 2, 29));
+        }
+
+        [Fact]
+        public void last_day_of_next_month_when_next_month_is_February_in_non_leap_year()
+        {
+            When
+                .ItIs("1997-01-20")
+                .Parsing("last day of next month")
+                .ReturnsSpan()
+                .StartingAt(Time.New(1997, 2, 28));
+        }
+
+        [Fact]
         public void last_day_of_this_month()
         {
             Parse("last day of this month")
